fa9c8ed8571f20801341f847f6c4164a2b370ec7
heoweb906 <heoweb906@gmail.com>
2025-04-10 12:15:20 +0900

    Squashed commit of the following:
    commit 300aba4a3a1738a47d1362243ef9e4dee74226a5
    Author: heoweb906 <heoweb906@gmail.com>
    Date:   Thu Apr 10 12:15:04 2025 +0900
    
        20250410 WOrk

--- Assets/Scripts/MapGimic/Chpater_1/Inside/Stage_1/SoundBlockMachine.cs
+++ Assets/Scripts/MapGimic/Chpater_1/Inside/Stage_1/SoundBlockMachine.cs
@@ -1,7 +1,10 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.AnimatedValues;
 using UnityEngine;
 using UnityEngine.EventSystems;
+
 
 public class SoundBlockMachine : ClockBattery, IPartsOwner
 {
@@ -12,12 +15,15 @@ public class SoundBlockMachine : ClockBattery, IPartsOwner
     public int[] iCorrectArray;
     private bool bScanFail;
 
+    public GameObject[] dials;
+    public GameObject[] levers;
+
 
     public override void TurnOnObj()
     {
         base.TurnOnObj();
 
-        RotateObject((int)fCurClockBattery);
+        RotateObject((int)fCurClockBattery + 2);
         nowCoroutine = StartCoroutine(PlayPitchSoundsCoroutine());
     }
     public override void TurnOffObj()
@@ -38,9 +44,16 @@ public class SoundBlockMachine : ClockBattery, IPartsOwner
     // #. 악기 작동
     private IEnumerator PlayPitchSoundsCoroutine()
     {
+        int iTempTime = (int)fCurClockBattery + 2;
         bScanFail = false;
 
-        while (fCurClockBattery > 0)
+        RotateZOverTime(dials[0], (int)fCurClockBattery + 2, true);
+        RotateZOverTime(dials[1], (int)fCurClockBattery + 2, false);
+
+        MoveYLoop(levers[0], -0.656f, -0.382f, 0.67f, 0.77f, fCurClockBattery + 2);
+        MoveYLoop(levers[1], -0.656f, -0.382f, 0.67f, 0.77f, fCurClockBattery + 2);
+
+        while (iTempTime > 0)
         {
             for (int i = 0; i < soundPieces.Length; i++)
             {
@@ -58,9 +71,10 @@ public class SoundBlockMachine : ClockBattery, IPartsOwner
                 yield return new WaitForSeconds(1.0f);
 
                 // 배터리 감소
-                fCurClockBattery -= 1;
-                if (fCurClockBattery <= 0)
+                iTempTime -= 1;
+                if (iTempTime <= 0)
                 {
+                    iTempTime = 0;
                     fCurClockBattery = 0;
                     TurnOffObj();
                     yield break; // 코루틴 종료
@@ -86,14 +100,82 @@ public class SoundBlockMachine : ClockBattery, IPartsOwner
     // #. 연주에 성공했을 때 실행시킬 함수
     private void SuccesPlayAction()
     {
-
+        Debug.Log("연주에 성공했습니다.");
     }
 
     // #. 연주에 실패했을 때 실행시킬 함수
     private void FailPlayAction()
     {
-
+        Debug.Log("연주에 실패했습니다.");
     }
+
+
+
+
+
+
+
+
+
+
+    public void RotateZOverTime(GameObject target, int duration, bool bRotateDir)
+    {
+        StartCoroutine(RotateZRoutine(target.transform, duration, bRotateDir));
+    }
+    private IEnumerator RotateZRoutine(Transform targetTransform, int duration, bool bRotateDir)
+    {
+        float timer = 0f;
+        float totalTime = duration;
+        float speed = 360f / totalTime; // 1초당 360도 회전
+
+        while (timer < totalTime)
+        {
+            float deltaRotation = speed * Time.deltaTime;
+            targetTransform.Rotate(0f, 0f, deltaRotation * (bRotateDir ? 1 : -1));
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+
+
+
+    public void MoveYLoop(GameObject target, float minY, float maxY, float minZ, float maxZ, float totalTime)
+    {
+        Debug.Log("여기서 실행해야 함");
+
+        StartCoroutine(MoveLeverManualRoutine(target.transform, minY, maxY, minZ, maxZ, totalTime));
+    }
+
+    private IEnumerator MoveLeverManualRoutine(Transform target, float minY, float maxY, float minZ, float maxZ, float totalTime)
+    {
+        float timer = 0f;
+        float moveSpeed = 1.5f;
+        float t = 0f;
+        bool forward = true;
+
+        Vector3 startPos = target.localPosition;
+        Vector3 from = new Vector3(startPos.x, minY, minZ);
+        Vector3 to = new Vector3(startPos.x, maxY, maxZ);
+
+        while (timer < totalTime)
+        {
+            t += Time.deltaTime * moveSpeed;
+            float easedT = Mathf.Sin(t * Mathf.PI * 0.5f); // InOutSine 느낌
+
+            target.localPosition = Vector3.Lerp(forward ? from : to, forward ? to : from, easedT);
+
+            if (easedT >= 1f)
+            {
+                t = 0f;
+                forward = !forward;
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
 
 
 
